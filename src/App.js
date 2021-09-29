310b26126282fe3e3646532649f5484bd189606b
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-09-29 16:31:36 +0900

    useState data 수정

--- src/App.js
+++ src/App.js
@@ -7,18 +7,25 @@ import './App.css';
 function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천','맛집 추천','공부리스트']); 
+  let [따봉,따봉변경] = useState(0);
   
-  // let posts = '강남 고기 맛집';
-  // let [day] = useState('9월 29일')   
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경( newArray );
+  }
+  
   return(
 
  
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
+        <button onClick={ 제목바꾸기 }>버튼</button>
       </div>
       <div className="list">
-        <h3> { 글제목[0] } </h3>
+        <h3> { 글제목[0] }<span onClick={ ()=>{따봉변경(따봉 +1) } }>👍</span> {따봉}</h3>
+        
         <p>2월 17일 발행</p>
         <hr/>
       </div>
