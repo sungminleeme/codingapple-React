bd873d21abc485bff9e3c56781148f2c26742930
tjdlas2412 <85213997+tjdlas2412@users.noreply.github.com>
2021-10-06 17:51:02 +0900

    input으로 state에 정보 넣기

--- src/App.js
+++ src/App.js
@@ -8,8 +8,13 @@ function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천','맛집 추천','공부리스트']); 
   let [따봉,따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
+  let [누른제목,누른제목변경] = useState(0);
 
-  let[modal, modal변경] = useState(false);
+  let [입력값, 입력값변경] = useState('');
+
+
+
 
   // let toggleMenu =() => {
   //   modal변경(modal => !modal);
@@ -35,43 +40,63 @@ function App() {
         <div>개발 Blog</div>
         <button onClick={ 글자순정렬 }>버튼</button>
       </div>
-      <div className="list">
-        <h3> { 글제목[0] }<span onClick={ ()=>{따봉변경(따봉 +1) } }>👍</span> {따봉}</h3>
-        
-        <p>2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3> { 글제목[1] } </h3>
-        <p>2월 18일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3 onClick={()=>{modal변경(true)}}> { 글제목[2] } </h3>
-        <p>2월 19일 발행</p>
-        <hr/>
-      </div>
-      
-      <button onClick={ ()=>{modal변경(!modal)}}>버튼</button>
 
+      { 
+        글제목.map(function(글, i){
+        return (
+        <div className="list">
+          <h3 onClick={ ()=>{누른제목변경(i)}}> {글} <span onClick={ ()=>{ 따봉변경(따봉+1)}}>👍 </span>
+          {따봉}</h3>
+          <p>2월 18일 발행</p>
+          <hr/>
+        </div>
+        )
+        })
+     }
+      {/* 인풋에 입력된 데이터를 저장해야됨 */}
+     <div className="publish">
+       <input onChange={(e) => {입력값변경(e.target.value)}}/>
+       <button onClick= {() =>{ 
+         var arrayCopy = [...글제목];
+         arrayCopy.unshift(입력값);
+         글제목변경(arrayCopy);
+        }}>저장</button>
+     </div>
+
+
+      <button onClick={ ()=>{modal변경(!modal)}}>열고닫기</button>
+        
       {
         modal === true
-        ? <Modal></Modal>
-        : null
+        ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal>
+        :null   
       }
+      
 
     </div>
   );
-  }  
+}  
 
-  function Modal(){
+  function Modal(props){
     return (
       <div className="modal">
-        <h2>제목</h2>
+        <h2>{props.글제목 [props.누른제목]}</h2>
         <p>날씨</p>
         <p>상세내용</p>
       </div>
     )
+  }
+
+  class Profile extends React.Component {
+    constructor() {
+      super();
+    }
+
+    render(){
+      return (
+        <div>프로필입니다</div>
+      )
+    }
   }
 
 
